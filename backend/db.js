ee450174cf7dc681e05a5c7ed473bb1c8295c7bd
Niko-transfem <shuheng.huang.2201@stu.moonshotacademy.cn>
2023-05-27 18:12:34 +0800

    login

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mysql = require('mysql')
 
-let db = mysql.createConnection({
+let connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
@@ -10,7 +10,24 @@ let db = mysql.createConnection({
 
 function login(username, password)
 {
-    return true;
+    //query database for matching entry
+    //todo: hashed and salted passwords
+    //todo: give the client a token instead of a simple true/false response
+    let stat = false;
+    console.log(username + " " + password + "\n");
+    connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+        console.log("callback is executed");
+        // If there is an issue with the query, output the error
+        if (error) throw error;
+        // If the account exists
+        if (results.length > 0) {
+            // Authenticate the user
+            stat = true;
+        } else {
+            stat = false;
+        }			
+    });
+    return stat;
 }
 
 function post(uid, title, content)
