75d7f712b1b009d7da8dcea046dae44ebef7cd3e
laladoko <hongsen.xu.2001@stu.moonshotacademy.cn>
2023-06-02 01:18:54 +0800

    image

--- backend/db.js
+++ backend/db.js
@@ -4,28 +4,35 @@ const mysql = require('mysql')
 let connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Lala050502',
-    database: 'test'
+    password: '',
+    database: 'msa_db'
 })
 
-db.query("SELECT username, password FROM users;", (err, data) => {
-    if (err) {
-        console.log("error!", err);
-    } else {
-        console.log("success!", data);
-
-        // 在成功后进行数据核对
-        const username = "desiredUsername";
-        const password = "desiredPassword";
-        const foundUser = data.find(user => user.username === 'uid');
-
-
-        if (foundUser) {
-            console.log("Credentials matched!");  // 用户名和密码匹配
-            return true;
+function login(username, password)
+{
+    //query database for matching entry
+    //todo: hashed and salted passwords
+    //todo: give the client a token instead of a simple true/false response
+    let stat = false;
+    console.log(username + " " + password + "\n");
+    connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+        console.log("callback is executed");
+        // If there is an issue with the query, output the error
+        if (error) throw error;
+        // If the account exists
+        if (results.length > 0) {
+            // Authenticate the user
+            stat = true;
         } else {
-            console.log("Invalid credentials.");  // 用户名和密码不匹配
-            return false;
-        }
-    }
-});
+            stat = false;
+        }			
+    });
+    return stat;
+}
+
+function post(uid, title, content)
+{
+    console.log("uid="+uid+" title=",title);
+}
+
+module.exports = {login, post};
