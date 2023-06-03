bbc2c3c0ae93e3510ee90a953b82ed0fd112b55f
laladoko <hongsen.xu.2001@stu.moonshotacademy.cn>
2023-06-04 05:24:21 +0800

    html完成

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,30 +1,29 @@
 <template>
-  <body>
-    <div class="center">
-    <div>
-      <img src="../image/login/titel_1.png" class="r_side">
-    </div>
-    <span class="r_side">账号</span>
-    <div class="kuang">
-      <input type="text" v-model="formData.uid" placeholder="请输入">
-    </div>
-    <span>密码</span>
-    <div>
-      <input type="password" v-model="formData.pwd">
-    </div>
-    <div class="button">
-      <input type="submit" value="登录" @click="login">
-    </div>
-  </div>
   <div>
-    <img src="../image/login/background_moonshot.png" class="background">
+    <div class="center">
+      <div>
+        <img src="../image/login/titel_1.png" class="r_side">
+      </div>
+      <span class="r_side">账号</span>
+      <div class="kuang">
+        <input type="text" v-model="formData.uid" placeholder="请输入账号/学号/邮箱">
+      </div>
+      <span>密码</span>
+      <div>
+        <input type="password" v-model="formData.pwd" placeholder="请输入密码">
+      </div>
+      <div >
+        <input type="submit" value="登录" @click="login" class="button">
+      </div>
+    </div>
+    <div>
+      <img src="../image/login/background_moonshot.png" class="background">
+    </div>
+      <img src="../image/login/background_1.png" class="light">
   </div>
-  </body>
-
 </template>
 
-
-<script setup> 
+<script setup>
 import http from '../api/http'
 const router = useRouter()
 const formData = reactive({
@@ -34,43 +33,68 @@ const formData = reactive({
 
 const userStore = useUserStore()
 let { token } = storeToRefs(userStore)
+
 const login = () => {
   http.post('/login', formData)
     .then(rep => {
       token.value = rep.data
-      // console.log(formData)
       if (token.value) router.push('/feed')
     })
 }
 </script>
 
-
 <style scoped>
-input{
+div{
+  margin: 0px;
+}
+input {
   border: none;
   border-bottom: 1px solid black;
   outline: none;
   width: 100%;
-
 }
-span{
+
+span {
   color: rgb(0, 0, 0);
-  font-size: 12px;
+  font-size: 20px; /* 修改字体大小为 20px */
   display: block;
+  margin: 60px 0px 0px 0px;
   margin-bottom: 20px;
   border: 292px 84px;
 }
+
 .center {
-  margin: 137px 45.5px 307px;
+  margin: 137px 45.5px 82px;
 }
 
-.r_side{
+.r_side {
   margin-left: 0;
 }
-.background{
+
+.button {
+  background-color: rgb(177, 214, 234);
+  color: white;
+  margin: 60px 0px 0px 0px;
+  height: 42.1px;
+  border-radius: 10px;
+}
+
+.background {
   margin-left: 18px;
   margin-right: 12px;
   margin-bottom: 54px;
   vertical-align: bottom;
+}
+.light{
+  content: '';
+  position: fixed;
+  top: 300px;
+  right: 0;
+  bottom: 0;
+  width: 50%; /* 设置伪元素的宽度为页面的一半 */
+  background-image: url('../image/login/background_1.png');
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
 }
 </style>
