3d960d9611cdcd8a2e2b80ef29517e8a1837ec67
laladoko <hongsen.xu.2001@stu.moonshotacademy.cn>
2023-06-09 14:48:35 +0800

    差后端链接

--- frontend/src/views/Profile.vue
+++ frontend/src/views/Profile.vue
@@ -1,25 +1,54 @@
 <template>
   <NavBar />
-  <div>{{ nickname }}</div>
-  <button @click="nickname = 11111"></button>
+  <div>info: {{ info }}</div>
   <button @click="logout">logout</button>
   <TabBar />
+
+  <post v-for="post in post_list" :key="post" :post-id="post" />
 </template>
 
 <script setup>
-const router = useRouter()
-const userStore = useUserStore()
-let { token } = storeToRefs(userStore)
+import axios from 'axios';
+let { user_id, token } = storeToRefs(useUserStore())
+
+const info = ref('')
+
 const logout = () => {
-  token.value = false
+  user_id.value = undefined
+  token.value = undefined
   router.push('/login')
 }
-let nickname = ref('name')
 
-// const info = ref('')
-// http.get('/profile?uid=' + uid.value).then(rep => {
-//   info.value = rep.data
-// })
+const post_list = ref([
+  {
+  name: "11111",
+  grade:"10",
+  time: "9:46",
+  content: "hahaha",
+  portrait: "IMG_9608_1.png",
+  likes: 0,
+  id: 0,
+  },
+  {
+
+  }
+]); // 用于存储post列表的数组
+
+axios.get(`/profile/${user_id.value}`)
+  .then(response => {
+    post_list.splice(0, post_list.length, ...rep.data); // 假设返回的数据是一个包含post列表的数组
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
+http.get(`/profile/${user_id.value}`)
+  .then(rep => {
+    info.value = rep.data
+  })
+  .catch(error => {
+    console.error(error);
+  });
 </script>
 
 <style scoped></style>
