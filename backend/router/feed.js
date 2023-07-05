9fa957a208cf320134c108f1088ea34e9107a8bf
laladoko <hongsen.xu.2001@stu.moonshotacademy.cn>
2023-07-05 12:34:49 +0800

    adjust

--- backend/router/feed.js
+++ backend/router/feed.js
@@ -43,25 +43,52 @@ router.post('/:id/unlike',verifyToken, (req, res) => {
     res.send(true)
   })
 })
-// router.post('/:id/comment', (req, res) => {
-//   const post_id = req.params.id
-//   const { content, user_id } = req.body
-//   db.query(`INSERT INTO comment (content, post_id, user_id, time) VALUES ('${content}', ${post_id}, ${user_id}, NOW());
-//             UPDATE post SET comments = comments + 1 WHERE id = ${post_id}`, (err) => {
-//     if (err) res.status(500).json({ err })
-//     else res.send(true)
-//   })
-// })
 
-// router.get('/detail/:id', verifyToken, (req, res) => {
-//   let post_id = req.params.id
-//   let user_id = res.locals.token
-//   db.query( `SELECT post.*, users.username, IF(likes.id, 1, 0) AS liked FROM post INNER JOIN user ON post.user_id = user.id
-//             LEFT JOIN likes ON like.post_id = post.id and likes.user_id = ${user_id} WHERE post.id = ${post_id};
-//             SELECT comment.*, users.username FROM comment JOIN user ON comment.user_id = user.id WHERE comment.post_id = ${post_id}`,(err, data) => {
-//     if (err) res.status(500).json({ err })
-//     else res.send(data)
-//   })
-// })
+
+router.post('/:id/comment', (req, res) => {
+  const post_id = req.params.id
+  const { content, user_id } = req.body
+  db.query(`INSERT INTO comment (content, post_id, user_id, times) VALUES ('${content}', ${post_id}, ${user_id}, NOW());
+            UPDATE post SET comments = comments + 1 WHERE id = ${post_id}`, (err) => {
+    if (err) res.status(500).json({ err })
+    else res.send(true)
+  })
+})
+
+
+router.get('/:id/comment', verifyToken, (req, res) => {
+  let user_id = res.locals.token;
+  let post_id = req.params.id;
+
+  // Construct the SELECT query with JOIN operation
+  const selectQuery = `
+    SELECT c.user_name, c.content, c.time, u.username
+    FROM comment AS c
+    INNER JOIN user AS u ON c.user_id = u.id
+    WHERE c.post_id = ${post_id} AND c.user_id = ${user_id}
+  `;
+
+  // Execute the SELECT query
+  db.query(selectQuery, (err, results) => {
+    if (err) {
+      res.status(500).json({ err });
+    } else {
+      // Send the query results as response
+      res.json(results);
+    }
+  });
+});
+
+
+
+
+router.get('/detail/:id', verifyToken, (req, res) => {
+  let post_id = req.params.id
+  let user_id = res.locals.token
+  db.query( `SELECT * FROM post WHERE id = '${post_id}' AND user_id = '${user_id}'`,(err, data) => {
+    if (err) res.status(500).json({ err })
+    else res.send(data)
+  })
+})
 
 module.exports = router
--- frontend/src/components/Comment.vue
+++ frontend/src/components/Comment.vue
@@ -10,7 +10,14 @@
 
 <script setup>
 import { reactive } from 'vue';
+import http from '../api/http';
 
+const formData = reactive({
+  post_id: '',
+  content: '',
+  user_id: '',
+  
+})
 const props = defineProps({
   comment: Object,
 })
@@ -18,7 +25,24 @@ const props = defineProps({
 
 const comment = reactive(props.comment);
 
+console.log(comment.$props)
+http.post('/deatil', )
+
+const commment_post = () => {
+  if (!content) {
+    msg.value = 'have no content!'
+    return
+  }
+  http.post('/feed/comment', formData).then(rep => {
+    console.log(2222222333333,rep.data.code)
+      user_id.value = rep.data.user_id
+      token.value = rep.data.token
+      content.value = rep.data.content
+  })
+}
+
 </script>
 
 <style scoped>
+  
 </style>
